74500763cf4cf82ce72bea009f84bfb7367ec390
unknown <volodin.kostia@gmail.com>
2022-03-18 14:14:26 -0400

    Update job.sh

--- implementation/job.sh
+++ implementation/job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=01:00:00
 #SBATCH --ntasks=256
-#SBATCH --mem=3gb
+#SBATCH --mem-per-cpu=3gb
 #SBATCH --job-name=optimsim
 #SBATCH --output=optimsim.out
 srun optimSimulation.py
