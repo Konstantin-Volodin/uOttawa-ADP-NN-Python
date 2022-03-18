f09ae6f40811c5feb19a19e0b5642ef18c660fde
unknown <volodin.kostia@gmail.com>
2022-03-18 11:21:24 -0400

    Update mpi.sh

--- implementation/mpi.sh
+++ implementation/mpi.sh
@@ -3,6 +3,6 @@
 #SBATCH --cpus-per-task=128
 #SBATCH --nodes=2
 #SBATCH --mem=0
-#SBATCH --job-name=optimsim
-#SBATCH --output=optim-optimsim.out
+#SBATCH --job-name=testmpi
+#SBATCH --output=optim-mpi.out
 mpirun -n 128 python3 mpi.py
