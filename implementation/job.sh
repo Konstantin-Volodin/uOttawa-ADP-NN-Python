3e58986b838e78dfd18dc87fc69563895b845af8
unknown <volodin.kostia@gmail.com>
2022-03-18 15:10:36 -0400

    error

--- implementation/job.sh
+++ implementation/job.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-#SBATCH --time=00:02:00
-#SBATCH --ntasks=256
-#SBATCH --mem-per-cpu=3gb
+#SBATCH --time=00:30:00
+#SBATCH --ntasks=10
+#SBATCH --mem-per-cpu=3G
 #SBATCH --job-name=optimsim
 #SBATCH --output=optimsim.out
+load module python/3.9.6 gurobi/9.5.0 mpi4py
+source /home/kvolodin/python-venv/bin/activate
 srun python3 optimSimulation.py
--- implementation/mpi.sh
+++ implementation/mpi.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-#SBATCH --time=00:30:00
-#SBATCH --ntasks=10
+#SBATCH --time=00:01:00
+#SBATCH --ntasks=128
 #SBATCH --mem-per-cpu=3G
 #SBATCH --job-name=testmpi
 #SBATCH --output=mpi.out
-load module python/3.9.6 gurobi/9.5.0 mpi4py
-source /home/kvolodin/python-venv/bin/activate
 srun python3 mpi.py
