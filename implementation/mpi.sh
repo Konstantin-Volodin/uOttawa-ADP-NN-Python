7d8d7ad79c494f28fd03d118c589f48adfbbae24
unknown <volodin.kostia@gmail.com>
2022-03-18 15:09:04 -0400

    Update mpi.sh

--- implementation/mpi.sh
+++ implementation/mpi.sh
@@ -4,4 +4,6 @@
 #SBATCH --mem-per-cpu=3G
 #SBATCH --job-name=testmpi
 #SBATCH --output=mpi.out
+load module python/3.9.6 gurobi/9.5.0 mpi4py
+source /home/kvolodin/python-venv/bin/activate
 srun python3 mpi.py
