d6419e60fdd802f0466d7eefee5a2e966bc2e88b
unknown <volodin.kostia@gmail.com>
2022-03-18 15:14:42 -0400

    Update job.sh

--- implementation/job.sh
+++ implementation/job.sh
@@ -4,6 +4,6 @@
 #SBATCH --mem-per-cpu=3G
 #SBATCH --job-name=optimsim
 #SBATCH --output=optimsim.out
-load module python/3.9.6 gurobi/9.5.0 mpi4py
+module load python/3.9.6 gurobi/9.5.0 mpi4py
 source /home/kvolodin/python-venv/bin/activate
 srun python3 optimSimulation.py
